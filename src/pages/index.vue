d67732644e210e3abb1f10285acd280060533833
소원진 <swj9077@gmail.com>
2024-05-06 20:06:59 +0900

    aws gateway api 연동 테스트

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,15 @@
 <script lang="ts" setup>
 import { onMounted } from "vue";
 
+async function search_text(keyword: string) {
+  const response = await fetch(
+    `https://n4vwwvk2m8.execute-api.us-east-1.amazonaws.com/text-embedding-stage/search?search-text=${keyword}`
+  );
+  const data = await response.json();
+  console.log("data", data);
+}
+
 onMounted(function () {
-  console.log("hi");
+  search_text("사과");
 });
 </script>
